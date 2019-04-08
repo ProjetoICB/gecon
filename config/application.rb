44fb56d1eab3335813ea47af6f46af1ff3dbc12b
Cesar <cps2005@gmail.com>
2019-04-08 16:49:29 -0300

    configuracao  geral feita

--- config/application.rb
+++ config/application.rb
@@ -24,12 +24,25 @@ module Gecon
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
+
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales','**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales= [:en, 'pt-BR']
+    config.i18n.default_locale = :'pt-BR'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address: "chagas.icb.usp.br",
+        port:     25,
+        domain:   "icb.usp.br"
+    }
   end
 end
