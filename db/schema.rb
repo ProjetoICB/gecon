6a724453d860e90fddf79c998bb763076fb1f58f
Cesar <cps2005@gmail.com>
2019-04-12 15:36:12 -0300

    fr index, layout e schema acertados

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190412170231) do
+ActiveRecord::Schema.define(version: 20190412175742) do
+
+  create_table "fontes_de_recurso", force: :cascade do |t|
+    t.string   "nome",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "tipos_de_compra", force: :cascade do |t|
     t.string   "nome",       limit: 255
