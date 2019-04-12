7e7db27c655b79e8d8497297fdb5a61ca3d94501
Cesar <cps2005@gmail.com>
2019-04-12 10:14:52 -0300

    refazer tipos de contas e grupos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410122551) do
+ActiveRecord::Schema.define(version: 20190410191007) do
 
   create_table "centros_de_custos", force: :cascade do |t|
     t.string   "nome",       limit: 255
