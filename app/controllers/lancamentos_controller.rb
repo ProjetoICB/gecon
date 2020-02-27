d59c1f030ad37a597e82271be4aa2afbb24f9464
Cesar <cps2005@gmail.com>
2020-02-27 14:03:53 -0300

    busca de lancamentos com modo mais simples

--- app/controllers/lancamentos_controller.rb
+++ app/controllers/lancamentos_controller.rb
@@ -319,7 +319,8 @@ class LancamentosController < ApplicationController
     if at == "Sim"
       @lancamentos = Lancamento.where("year(created_at) = ?", Date.today.year).order("id desc")
     else
-      @lancamentos = Lancamento.all.order("id desc")
+      #@lancamentos = Lancamento.all.order("id desc")
+      @lancamentos = Lancamento.includes(:conta).includes(:fornecedor).includes(:item_de_despesa).includes(:item_de_receita).includes(:tipo_de_compra).includes(:transferencia).order("id desc")
     end
   end
 
